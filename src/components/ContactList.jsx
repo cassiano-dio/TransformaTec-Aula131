a599437d77c618e377060a0a682a65c92c21af8d
cassiano-dio <cassiano@dio.me>
2022-09-08 09:43:15 -0300

    Novo commit

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -5,14 +5,19 @@ const ContactList = () => {
     const [contactList, setContactList] = useState([]);
     const [contactId, setContactId] = useState('');
 
-    let url = 'http://localhost:8080/api/users/1/contacts';
+    const url = "http://localhost:8080/api/contacts/1/phones"
 
-    useEffect(() => {
+    useEffect(() =>{
         fetch(url)
         .then(
-            (result) => console.log(result),
+            (result) => result.json()
+        ).then(
+            (data) => console.log(data[0].contact.user)
+        ).catch(
+            (err) => console.log(`Error: ${err}`)
         )
-    })
+
+    },[])
 
     return(
         <div>
